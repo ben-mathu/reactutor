e79f0d180a536c5aca7ac7077d71a8147f66cb25
bmrichie <mathubenard@gmail.com>
2017-11-22 17:42:43 +0300

    Created add and substraction buttons. css file

--- Main.js
+++ Main.js
@@ -1,42 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
-import {NewC} from './NewC'
 
 class Hello extends React.Component {
-
-	//Defining state in a Component
-	constructor() {
-
-		super();
-		this.state = {Handle: "b_richie"}
-	}
 
 	render() {
 		const pi = 3.14;
 		const weekdays = ["Mon","Tue","Wed","Thus","Fri"];
-		const today = "Mon  22nd November, 2017";
+		const today = "Mon  21st November, 2017";
 
 		//JSX code in return function
 		return (
 
 			<div>
 				<h1>Hello Topcoder Community!</h1>
-				<h2>My Topcoder handle is {this.state.Handle}</h2>
-
 				<a className="TC">The value of pi is: {pi}</a>
-
 				<ul>Weekdays:- <br/>
-					{weekdays.map((weekdays, index) => (
-						<li key={index}>{weekdays}</li>
-					))}
+					{weekdays.map(day => <li>{day}</li>)}
 				</ul>
-
 				<div>
-					Date:- <b>{today}</b>
+					Today:- <b>{today}</b>
 				</div>
-
-				<NewC Name={"TOPCODER"} pi = {pi}/>
-
 			</div>
 		);
 	}
